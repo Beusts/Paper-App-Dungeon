ca5f4fd99eb2c826f542808bc3e794601f456baf
Beusts <sonic18@outlook.fr>
2025-01-08 01:51:11 +0100

    fix: Ajustement des dimensions de la fenêtre pour que la largeur soit toujours un multiple de 15

--- code/settings.py
+++ code/settings.py
@@ -3,11 +3,12 @@ import sys
 from os.path import join
 from os import walk
 
-# Dimensions de la fenêtre
-WINDOW_HEIGHT = 720
-WINDOW_WIDTH = int(WINDOW_HEIGHT * (9 / 16))
+# Dimensions de la hauteur de la fenêtre
+WINDOW_HEIGHT = 720 * 1.6
 # Taille d'une tuile
-TILE_SIZE = WINDOW_WIDTH // 15
+TILE_SIZE = int(WINDOW_HEIGHT * (9 / 16)) // 15
+# Dimensions de la largeur de la fenêtre
+WINDOW_WIDTH = TILE_SIZE * 15
 # Fréquence d'images par seconde
 FPS = 60
 # Temps de sommeil entre les mouvements
