3899248f089201f57d637175e8087c9aa4476f88
qpayet <quentin.payet@edu.univ-fcomte.fr>
2025-01-27 17:53:31 +0100

    [ADD] wallGenerator class : testing generation

--- code/wallGenerator.py
+++ code/wallGenerator.py
@@ -5,11 +5,113 @@ from random import randint
 
 class WallGenerator(pygame.sprite.Sprite):
 
-    def __init__(self, current_object_on_level, wall_pourcentage, level_size):
+    def __init__(self, current_object_on_level, wall_pourcentage, level_size, generation = 3):
         super().__init__()
         self.wall_pourcentage = wall_pourcentage
         self.current_object_on_level = current_object_on_level
         self.level_size_rows, self.level_size_cols = level_size
+        self.generation = generation
+
+
+    # def generate_walls(self, block):
+    # 
+    #     for i in range(self.generation):
+    #         block = self.divide_part(block)
+    # 
+    #     return self.current_object_on_level
+    # 
+    # def divide_part(self, block):
+    #     self.generate_wall_arround_level()
+    # 
+    #     x, y, width, height = block
+    # 
+    #     _len = x + width
+    #     large = y + height
+    # 
+    #     orientation = self.chosing_orientation()
+    #     wall_x = x
+    #     wall_y = y
+    # 
+    #     walls = []
+    # 
+    #     if orientation == "vertical":
+    #         wall_x = randint(x + width // 4, x + width - width // 4)
+    #         for i in range(large):
+    #             walls.append((wall_x, wall_y + i))
+    #     else:
+    #         wall_y = randint(y + height // 4, y + height - height // 4)
+    # 
+    #         for i in range(large):
+    #             walls.append((wall_x + i, wall_y))
+    # 
+    #     print(walls)
+    #     self.add_block(walls)
+    #     self.making_gap(walls, orientation)
+    # 
+    # def chosing_orientation(self):
+    #     res = randint(1, 2)
+    # 
+    #     if res == 1:
+    #         return "vertical"
+    #     return "horizontal"
+    # 
+    # def generate_wall_arround_level(self, ):
+    #     for row in range(15):
+    #         for col in range(15):
+    # 
+    #             # Place walls all around the map
+    #             if row == 0 or row == self.level_size_rows - 1 or col == 0 or col == self.level_size_cols - 1:
+    #                 self.current_object_on_level.append((row, col))
+    #                 continue
+    # 
+    # def add_block(self, wall):
+    #     for i in range(len(wall)):
+    #         if self.is_valid_wall(wall[i]):
+    #             self.current_object_on_level.append(wall[i])
+    # 
+    # def is_valid_wall(self, wall):
+    # 
+    #     for i in range(self.level_size_rows):
+    #         for i in range(self.level_size_cols):
+    #             if wall[0] == 0 or wall[1] == 0 \
+    #                 or wall[0] == self.level_size_rows - 1 or wall[1] == self.level_size_cols - 1:
+    #                 return False
+    # 
+    #     if wall in self.current_object_on_level:
+    #         return False
+    # 
+    #     return True
+    # 
+    # def making_gap(self, walls, orientation):
+    # 
+    #     size_gap = randint(1, 4)
+    # 
+    #     while True:
+    #         deleted_wall_index = randint(0, len(walls) - 1)
+    #         deleted_wall = walls[deleted_wall_index]
+    # 
+    #         if deleted_wall in self.current_object_on_level and not(\
+    #                 deleted_wall[0] == 0 or deleted_wall[1] == 0 \
+    #                 or deleted_wall[0] == self.level_size_rows - 1 or deleted_wall[1] == self.level_size_cols - 1):
+    #             break
+    # 
+    #     for i in range(size_gap):
+    # 
+    #         if orientation == "vertical":
+    #             next = deleted_wall[0], deleted_wall[1] + i
+    # 
+    #             if next not in self.current_object_on_level:
+    #                 continue
+    # 
+    #             self.current_object_on_level.remove(next)
+    # 
+    #         else:
+    #             next = deleted_wall[0] + i, deleted_wall[1]
+    # 
+    #             if next not in self.current_object_on_level:
+    #                 continue
+    # 
+    #             self.current_object_on_level.remove(next)
 
     def generate_wall_level(self):
         size_level = self.level_size_rows * self.level_size_cols
@@ -60,3 +162,18 @@ class WallGenerator(pygame.sprite.Sprite):
         for i in range(block["size"][0]):
             for j in range(block["size"][1]):
                 self.current_object_on_level.append( (block["position"][0] + i, block["position"][1] + j) )
+
+# if __name__ == '__main__':
+    # wall = WallGenerator([], 0.15, (15, 15))
+    # walls = wall.divide_part((1, 1, 13, 13))
+    #
+    # _str = ""
+    # for i in range(15):
+    #     for j in range(15):
+    #         if (i, j) in wall.current_object_on_level:
+    #             _str += "1"
+    #         else:
+    #             _str += "0"
+    #     _str += "\n"
+    #
+    # print(_str)
