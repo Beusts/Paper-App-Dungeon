512a0e1ec27afe8196066835dea67bc1dbb6c0fb
Beusts <sonic18@outlook.fr>
2025-02-16 00:54:41 +0100

    [FIX] Amélioration de la logique de génération des fichiers PDF en simplifiant l'indexation des niveaux et en centrant les images sur la page A4

--- code/pdfGenerator.py
+++ code/pdfGenerator.py
@@ -20,27 +20,23 @@ class PdfGenerator(pygame.sprite.Sprite):
 
         screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-        shop_index = 0
-        level_index = 0
         nb_files = len(level_files)
 
-        for frame_count in range(nb_files):
+        for i in range(nb_files):
 
-            current_stage = Level(f"{level_files[level_index]}", self.player)
-
-            if level_files[level_index].startswith('shop'):
-                self.current_stage = Shop(
-                    level_files[level_index], self.player)
+            if level_files[i].startswith('shop'):
+                current_stage = Shop(
+                    level_files[i], self.player)
             else:
-                self.current_stage = Level(
-                    level_files[level_index], self.player)
+                current_stage = Level(
+                    level_files[i], self.player)
 
             current_stage.run(dt)
             pygame.display.update()
 
             # Screen shot in memory
             frame_path = os.path.join(
-                save_path, f"frame_{frame_count:03d}.png")
+                save_path, f"frame_{i:03d}.png")
             pygame.image.save(screen, frame_path)
             pygame.display.update()
 
@@ -50,7 +46,8 @@ class PdfGenerator(pygame.sprite.Sprite):
         for image_file in image_files:
             img_path = os.path.join(save_path, image_file)
             pdf.add_page()
-            pdf.image(img_path, x=10, y=10, w=180)  # manage image size in PDF
+            # manage image size in PDF, image centered on A4 page
+            pdf.image(img_path, x=15, y=10, w=180)
 
             if os.path.exists(save_path):
                 os.remove(img_path)
