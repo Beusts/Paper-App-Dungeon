d08f8a58101d5c2b4f334787300349c15bc55071
Beusts <sonic18@outlook.fr>
2025-03-12 18:36:46 +0100

    [DOC] Ajoute la doc dans main.py

--- code/main.py
+++ code/main.py
@@ -40,30 +40,42 @@ class Game:
         random.seed(seed)
         print("Seed pour la génération des niveaux : ", seed)
 
+        # Génère une séquence alternée de niveaux et de magasins
         for i in range(total_iterations):
-            # Si on est à l'intervalle pour un shop et qu'il reste des shops à ajouter…
+            # Si on est à l'intervalle pour un shop et qu'il reste des shops à ajouter
             if shop_count < total_shops and (i + 1) % shop_interval == 0:
+                # Création d'un fichier CSV pour le magasin avec 3 articles
                 create_shop_csv_file(f'shop{shop_count}', 3)
                 self.level_map_files.append(f'shop{shop_count}')
                 shop_count += 1
             else:
                 level_count += 1
+                # Augmente la difficulté tous les 10 niveaux
                 if level_count % 10 == 0 and level_count != 0:
                     difficulty += 1
-            create_maze_csv_file(f'level{level_count}', 15, 15, difficulty)
-            self.level_map_files.append(f'level{level_count}')
+                # Création d'un labyrinthe de 15x15 avec la difficulté actuelle
+                create_maze_csv_file(f'level{level_count}', 15, 15, difficulty)
+                self.level_map_files.append(f'level{level_count}')
 
+        # Initialisation du joueur
         self.player = Player()
 
+        # Initialisation du premier niveau
         self.current_stage = Level(
             self.level_map_files[self.current_level_index], self.player)
 
+        # Nouvelle graine aléatoire pour les mouvements dans le jeu
         seed = random.SystemRandom().random()
         random.seed(seed)
         print("Seed pour les mouvements : ", seed)
 
     def change_level(self):
-
+        """
+        Passe au niveau suivant dans la séquence.
+        Si le niveau est un magasin, instancie un objet Shop.
+        Si le niveau est un labyrinthe, instancie un objet Level.
+        Si tous les niveaux ont été complétés, termine le jeu.
+        """
         self.current_level_index += 1
 
         if self.current_level_index >= len(self.level_map_files):
